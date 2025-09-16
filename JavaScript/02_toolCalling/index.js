34eddf72009f75d902a21b624f78e38a1ec4bebf
cmrohityadav <cmrohityadav23@gmail.com>
2025-09-16 22:49:42 +0530

    added function llm will call continuosly to get answer

--- JavaScript/02_toolCalling/index.js
+++ JavaScript/02_toolCalling/index.js
@@ -1,117 +1,97 @@
 import dotenv from 'dotenv'
 dotenv.config();
 import Groq from 'groq-sdk'
-import {tavily} from "@tavily/core"
+import { tavily } from "@tavily/core"
 
 const client = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-const tvly = tavily({apiKey:process.env.TAVILY_API_KEY});
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
+
 async function main() {
 
-    const messages=[
-            {
-                role: 'system',
-                content: `You are smart personal assistant who answer the asked question .
+    const messages = [
+        {
+            role: 'system',
+            content: `You are smart personal assistant who answer the asked question .
                  You have access to following tools:
                  1. webSearch({query}:{query:string}) //Search the latest information and  realtime data on the internet`,
-            },
-            {
-                role: 'user',
-                content: 'when was iphone 17 was launched'
-            }
-        ];
-
-    
-    const completion = await client.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
-        temperature: 0,
-        messages:messages,
-        tools: [
-            {
-                type: 'function',
-                function: {
-                    name: 'webSearch',
-                    description: 'Search the latest information and  realtime data on the internet',
-                    parameters: {
-                        type: 'object',
-                        properties: {
-                            query: {
-                                type: 'string',
-                                description: 'The search query to perform search on'
-                            }
-                        },
-                        required: ["query"]
-                    },
-
-                },
-
-            }
-        ],
-        tool_choice: 'auto'
-        
-    });
-
-    messages.push(completion.choices[0].message);
-
-    const toolCalls = completion.choices[0].message.tool_calls;
-
-
-
-    if (!toolCalls) {
-        console.log(`Assistant: ${completion.choices[0].message.content}`);
-        return;
-    }
-
-    for (const tool of toolCalls) {
-        console.log('tool: ', tool);
-
-        const functionName = tool.function.name;
-        const functionParams = tool.function.arguments;
-
-        if (functionName == 'webSearch') {
-            const toolResult = await webSearch(JSON.parse(functionParams));
-            console.log("Tool result: ", toolResult);
-
-            messages.push({
-                tool_call_id:tool.id,
-                role:'tool',
-                name:functionName,
-                content:toolResult,
-            })
+        },
+        {
+            role: 'user',
+            content: 'current LTP of TCS stock today from nse'
+            // 'when was iphone 17 was launched'
         }
+    ];
+
+    while (true) {
+        const completion = await client.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            temperature: 0,
+            messages: messages,
+            tools: [
+                {
+                    type: 'function',
+                    function: {
+                        name: 'webSearch',
+                        description: 'Search the latest information and  realtime data on the internet',
+                        parameters: {
+                            type: 'object',
+                            properties: {
+                                query: {
+                                    type: 'string',
+                                    description: 'The search query to perform search on'
+                                }
+                            },
+                            required: ["query"]
+                        },
+
+                    },
+
+                }
+            ],
+            tool_choice: 'auto'
+
+        });
+
+        messages.push(completion.choices[0].message);
+
+        const toolCalls = completion.choices[0].message.tool_calls;
+
+
+
+        if (!toolCalls) {
+            console.log(`Assistant: ${completion.choices[0].message.content}`);
+            break;
+        }
+
+        for (const tool of toolCalls) {
+            console.log('tool: ', tool);
+
+            const functionName = tool.function.name;
+            const functionParams = tool.function.arguments;
+
+            if (functionName == 'webSearch') {
+                const toolResult = await webSearch(JSON.parse(functionParams));
+                console.log("Tool result: ", toolResult);
+
+                messages.push({
+                    tool_call_id: tool.id,
+                    role: 'tool',
+                    name: functionName,
+                    content: toolResult,
+                })
+            }
+        }
+
     }
 
 
 
-    const completion2 = await client.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
-        temperature: 0,
-        messages:messages,
-        tools: [
-            {
-                type: 'function',
-                function: {
-                    name: 'webSearch',
-                    description: 'Search the latest information and  realtime data on the internet',
-                    parameters: {
-                        type: 'object',
-                        properties: {
-                            query: {
-                                type: 'string',
-                                description: 'The search query to perform search on'
-                            }
-                        },
-                        required: ["query"]
-                    },
 
-                },
 
-            }
-        ],
-        tool_choice:'auto'
-    });
 
-    console.log("Final response : ", JSON.stringify(completion2.choices[0].message, null, 2));
+
+    // console.log("Final response : ", JSON.stringify(completion2.choices[0].message, null, 2));
 
 
 
@@ -138,13 +118,13 @@ await main();
 async function webSearch({ query }) {
     console.log("Calling web search...........");
 
-    const response=await tvly.search(query);
+    const response = await tvly.search(query);
 
     // return "Iphone was launched on 20 september 2024"
 
-    console.log("searched response: ",response);
+    console.log("searched response: ", response);
 
-    const finalResult=response.results.map((result)=>result.content).join("\n\n");
+    const finalResult = response.results.map((result) => result.content).join("\n\n");
 
     return finalResult;
 }
